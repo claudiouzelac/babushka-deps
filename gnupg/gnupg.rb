18f717e71931965ec0cfac961a6017c086b73799
Stewart Henderson <shenderson@mozilla.com>
2019-07-25 15:27:49 -0500

    Added GPG support

---
+++ gnupg/gnupg.rb
@@ -0,0 +1,26 @@
+dep 'gpg_directory' do
+    def gnu_directory
+      '~/.gnupg/'
+    end
+    met? {
+      gnu_directory.p.exists?
+    }
+    meet {
+      shell("mkdir -p #{gnu_directory}")
+      shell("sudo chmod -R `whoami` #{gnu_directory}")
+    }
+end
+  
+dep 'gpg_configuration' do
+    requires 'gpg_directory'
+    def config_file
+      '~/.gnupg/gpg.conf'
+    end
+  
+    met? {
+      Babushka::Renderable.new(config_file).from?('gnupg/gnupg.conf.erb')
+    }
+    meet {
+      render_erb 'gnupg.conf.erb', :to => config_file
+    }
+end
--- mozilla.rb
+++ mozilla.rb
@@ -2,6 +2,7 @@ dep "mozilla laptop bootstrap" do
     requires [
         'git_configured'.with(:git_username => 'Stewart Henderson', :git_email => 'shenderson@mozilla.com'),
         'bash_configuration',
+        'gpg_configuration',
         'mozilla devops',
         'mozilla subhub'
     ]
