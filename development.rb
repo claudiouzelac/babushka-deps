bb47966232f02814e601b4233e6ae66ac59c8cb9
Stewart Henderson <shenderson@mozilla.com>
2019-07-25 14:04:22 -0500

    Added initial dotfiles.

--- development.rb
+++ development.rb
@@ -1,11 +1,21 @@
 dep "stripe-mock.managed" do provides "stripe-mock" end
 dep "cmake.managed" do provides "cmake" end
 dep "node.managed" do provides "node" end
+dep "yarn.managed" do provides "yarn" end
+dep "bash.managed" do provides "bash" end
+dep "zsh.managed" do provides "zsh" end
+
+dep "shenderson terminal" do
+    requires "bash.managed"
+    requires "zsh.managed"
+    requires "shenderson dotfiles"
+end
 
 dep "mozilla subhub" do
   requires "python"
   requires "stripe-mock.managed"
   requires "node.managed"
+  requires "yarn.managed"
   requires "repository".with  :path => "~/workspace/subhub",
     :url => "git@github.com:mozilla/subhub.git",
     :branch => "master"
