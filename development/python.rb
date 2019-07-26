42c3b20671e34c903e552226461db154a288ff28
Stewart Henderson <shenderson@mozilla.com>
2019-07-26 06:59:23 -0400

    added alias for aws login

--- development/python.rb
+++ development/python.rb
@@ -1,5 +1,7 @@
 dep "pycharm.cask"
 dep "pyenv.managed" do installs 'pyenv' end
+# https://hackernoon.com/reaching-python-development-nirvana-bb5692adf30c
+dep "pipenv.managed" do installs 'pipenv' end
 
 dep 'python.managed' do
     requires { on :osx, 'homebrew' }
@@ -10,6 +12,7 @@ dep 'python' do
     requires [
         'python.managed',
         'pycharm.cask',
-        'pyenv.managed'
+        'pyenv.managed',
+        'pipenv.managed'
     ]
 end
